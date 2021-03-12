09a4936c3ad4dcf109c926850287a59de52acd32
chandrasaibhanukumar <bhanukumar.kakumani@gmail.com>
2021-03-12 14:07:00 +0100

    This is my code from git local

---
+++ pratice2/src/package2/Bhanu2.java
@@ -0,0 +1,17 @@
+package package2;
+
+public class Bhanu2 {
+
+	public static void main(String[] args) {
+		System.out.println("Kumar");
+		System.out.println("Kumar");
+		System.out.println("Kumar");
+		System.out.println("Kumar");
+		System.out.println("Kumar");
+		System.out.println("Kumar");
+		System.out.println("Kumar");
+
+
+	}
+
+}
